266168d85ea90904614d249eb9b95febfb2079ec
Caleb Fletcher <caleb@fletcher.cf>
2021-10-18 22:58:34 +1100

    feat: add better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum SudokuError {
+    #[error("unsolvable sudoku")]
+    Unsolvable,
+}
+
 #[derive(Copy, Clone)]
 pub struct Sudoku {
     cells: [[i32; 9]; 9],
@@ -8,6 +16,8 @@ impl Sudoku {
         Self { cells }
     }
 
-    pub fn solve(self: &mut Self) {}
+    pub fn solve(self: &mut Self) -> Result<(), anyhow::Error> {
+        Ok(())
+    }
     pub fn print(self: &Self) {}
 }
