dea22b5ac056febd690e5fc76b7ff2dcb9e11633
Caleb Fletcher <caleb@fletcher.cf>
2021-10-19 12:20:56 +1100

    test: add sudoku creation tests

---
+++ tests/creation_test.rs
@@ -0,0 +1,37 @@
+use sudoku_solver::{Sudoku, SudokuError};
+
+#[test]
+fn it_creates_valid_sudoku() {
+    let sudoku = Sudoku::new([
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]);
+
+    assert!(sudoku.is_ok());
+}
+
+#[test]
+fn it_fails_to_create_invalid_sudoku() {
+    // Two 3's in top row
+    let sudoku = Sudoku::new([
+        [5, 3, 3, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]);
+    assert!(sudoku.is_err());
+    let err = sudoku.unwrap_err();
+    assert!(matches!(err, SudokuError::Invalid));
+}
